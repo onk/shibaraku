f6acd25dcc9a12d01d717b5085f1fc9305c1d6a5
Takafumi ONAKA <takafumi.onaka@gmail.com>
2014-11-18 04:17:26 +0900

    add null pattern test

--- spec/active_record_ext_spec.rb
+++ spec/active_record_ext_spec.rb
@@ -5,25 +5,34 @@ describe Shibaraku::ActiveRecordExt do
     @now     = Time.now
     @old     = @now - 1.minute
     @future  = @now + 1.minute
-    @old_old       = Campaign.create(start_at: @old,    end_at: @old)
-    @old_now       = Campaign.create(start_at: @old,    end_at: @now)
-    @old_future    = Campaign.create(start_at: @old,    end_at: @future)
-    @now_now       = Campaign.create(start_at: @now,    end_at: @now)
-    @now_future    = Campaign.create(start_at: @now,    end_at: @future)
-    @feture_feture = Campaign.create(start_at: @future, end_at: @future)
+    @records = []
+    @records << @null_null     = Campaign.create(start_at: nil,     end_at: nil)
+    @records << @null_old      = Campaign.create(start_at: nil,     end_at: @old)
+    @records << @null_now      = Campaign.create(start_at: nil,     end_at: @now)
+    @records << @null_future   = Campaign.create(start_at: nil,     end_at: @future)
+    @records << @old_null      = Campaign.create(start_at: @old,    end_at: nil)
+    @records << @now_null      = Campaign.create(start_at: @now,    end_at: nil)
+    @records << @future_null   = Campaign.create(start_at: @future, end_at: nil)
+    @records << @old_old       = Campaign.create(start_at: @old,    end_at: @old)
+    @records << @old_now       = Campaign.create(start_at: @old,    end_at: @now)
+    @records << @old_future    = Campaign.create(start_at: @old,    end_at: @future)
+    @records << @now_now       = Campaign.create(start_at: @now,    end_at: @now)
+    @records << @now_future    = Campaign.create(start_at: @now,    end_at: @future)
+    @records << @feture_feture = Campaign.create(start_at: @future, end_at: @future)
   end
 
   describe ".in_time(now)" do
     it "find from <= now < to" do
-      expect(Campaign.in_time.to_a).to eq [@old_future, @now_future]
+      expect(Campaign.in_time.to_a).to eq [@null_null, @null_future, @old_null, @now_null,
+                                           @old_future, @now_future]
     end
   end
 
   describe "#in_time?(now)" do
     it "return from <= now < to" do
-      arr = [@old_old, @old_now, @old_future, @now_now, @now_future, @feture_feture]
-      in_time_arr = arr.select { |e| e.in_time?(@now) }
-      expect(in_time_arr).to eq [@old_future, @now_future]
+      in_time_arr = @records.select { |e| e.in_time?(@now) }
+      expect(in_time_arr).to eq [@null_null, @null_future, @old_null, @now_null,
+                                 @old_future, @now_future]
     end
   end
 end
