31baf53a45da17a60e7770aeedea1ba20eaa58ac
Takafumi ONAKA <takafumi.onaka@gmail.com>
2014-11-18 03:47:23 +0900

    use rspec

--- shibaraku.gemspec
+++ shibaraku.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |s|
   s.add_dependency "rails"
 
   s.add_development_dependency "mysql2"
+  s.add_development_dependency "rspec"
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require "shibaraku"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
