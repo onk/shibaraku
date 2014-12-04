3832ce74835a38ee34ee0624b24a616c2896c39b
Takafumi ONAKA <takafumi.onaka@gmail.com>
2014-12-04 18:41:32 +0900

    テスト実行時にデータ書き込んでたので example ごとに rollback するように

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,15 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+
+      # テストデータを初期化するためにロールバックする
+      raise ActiveRecord::Rollback
+    end
+  end
 end
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
