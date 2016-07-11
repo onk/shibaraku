b9e3d82df339b9c6a692b7d82a6e70175df8603d
Takafumi ONAKA <takafumi.onaka@gmail.com>
2016-07-12 06:44:49 +0900

    add test using has_many :through
    user has_many departments, through user_departments
    
        +--------+        +-------------+
        | User   |        | Department  |
        |        |        | (shibaraku) |
        +------+-+        +-+-----------+
               |            |
             +-+------------+--+
             |  UserDepartment |
             |  (shibaraku)    |
             +-----------------+

--- spec/support/create_test_database.rb
+++ spec/support/create_test_database.rb
@@ -14,6 +14,22 @@ ActiveRecord::Schema.define version: 0 do
     t.datetime :started_at
     t.datetime :ended_at
   end
+
+  create_table :users, force: true do |t|
+    t.string   :name, null: false
+  end
+  create_table :departments, force: true do |t|
+    t.string   :name, null: false
+    t.datetime :start_at
+    t.datetime :end_at
+  end
+  create_table :user_departments, force: true do |t|
+    t.integer  :user_id,       null: false
+    t.integer  :department_id, null: false
+    t.datetime :start_at
+    t.datetime :end_at
+    t.index    [:user_id, :department_id]
+  end
 end
 
 class Campaign < ActiveRecord::Base
@@ -22,4 +38,25 @@ end
 
 class Event < ActiveRecord::Base
   shibaraku start_at: :started_at, end_at: :ended_at
+end
+
+class User < ActiveRecord::Base
+  has_many :user_departments
+  has_many :current_user_departments, -> { in_time }, class_name: "UserDepartment"
+  has_many :departments, through: :user_departments
+  has_many :current_departments, through: :current_user_departments, source: :department
+end
+
+class Department < ActiveRecord::Base
+  shibaraku
+  has_many :user_departments
+  has_many :current_user_departments, -> { in_time }, class_name: "UserDepartment"
+  has_many :users, through: :user_departments
+  has_many :current_users, through: :current_user_departments, source: :user
+end
+
+class UserDepartment < ActiveRecord::Base
+  shibaraku
+  belongs_to :user
+  belongs_to :department
 end
